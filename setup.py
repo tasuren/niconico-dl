58efae4c5d8f71dda62ec28064a30e063a952b81
HIDE <48782538+HIDE810@users.noreply.github.com>
2021-09-21 11:23:07 +0900

    Fix version and author

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ else:
 
 with open("niconico_dl/__init__.py") as f:
     text = f.read()
-    version = text.split("__version__ = ")[1].split("\n")[0]
-    author = text.split("__author__ = ")[1].split("\n")[0]
+    version = text.split('__version__ = "')[1].split('"')[0]
+    author = text.split('__author__ = "')[1].split('"')[0]
 
 setup(
     name='niconico_dl',
